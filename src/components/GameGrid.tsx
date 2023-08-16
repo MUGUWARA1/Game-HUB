4dc4040cf982684143e06b5f94cdf88eb46c2357
DEROUICH KARIM <106762744+MUGUWARA1@users.noreply.github.com>
2023-08-16 16:58:36 +0100

    refactor and remove dupicate styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,22 +1,37 @@
-import {Center, SimpleGrid, Text } from "@chakra-ui/react"
-import useGames from "../hooks/useGames"
-import GameCard from "./GameCard"
-import GamecardSkeleton from "./GamecardSkeleton"
-
+import { Center, SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
+import GamecardSkeleton from "./GamecardSkeleton";
+import GameCardContaner from "./GameCardContainer";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-   const {games,error,loading} = useGames()
-   const sekelton =[0,1,2,3,4,5,6,7,8,9]
+  const { games, error, loading } = useGames();
+  const sekelton = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
     <>
-    {error && <Text color={'red'}>{error}</Text>}
-        <SimpleGrid columns={{sm:1 , md:2 , lg:3 ,xl:4}} spacing={10} padding={'10px'} justifyContent={'Center'}>
-            {loading && sekelton.map(sk=><GamecardSkeleton></GamecardSkeleton>)}
-            {games.map(game =><GameCard  key={game.id} game={game} ></GameCard>)}
-        </SimpleGrid>
+      {error && <Text color={"red"}>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={10}
+        padding={"10px"}
+        justifyContent={"Center"}
+      >
+        {loading &&
+          sekelton.map((sk) => (
+            <GameCardContainer>
+              <GamecardSkeleton></GamecardSkeleton>
+            </GameCardContainer>
+          ))}
+        {games.map((game) => (
+          <GameCardContainer>
+            <GameCard key={game.id} game={game}></GameCard>
+          </GameCardContainer>
+        ))}
+      </SimpleGrid>
     </>
-  )
-}
+  );
+};
 
-export default GameGrid
+export default GameGrid;
