66e9f576ff30d9918994fc367c35fd2a97e1377b
DEROUICH KARIM <106762744+MUGUWARA1@users.noreply.github.com>
2023-08-17 16:16:59 +0100

    template more responsive and display the genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
-import NavBar from './components/NavBar'
-import GameGrid from './components/GameGrid'
-import Genres from './components/Genres'
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
+import Genres from "./components/Genres";
 function App() {
-
   return (
-    <Grid templateAreas={{ base:`"nav" "main"`,lg: `"nav nav " "aside main"`}}>
-      <GridItem area={"nav"} >
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav " "aside main"` }}
+      templateColumns={{ base: "1fr", lg: "200px 1fr" }}
+    >
+      <GridItem area={"nav"}>
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area={"aside"} ><Genres></Genres></GridItem>
+        <GridItem area={"aside"} paddingX={5}>
+          <Genres></Genres>
+        </GridItem>
       </Show>
-      <GridItem area={"main"} >
+      <GridItem area={"main"}>
         <GameGrid></GameGrid>
       </GridItem>
     </Grid>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Genres.tsx
+++ src/components/Genres.tsx
@@ -1,14 +1,21 @@
 import useGenres from "../hooks/useGenres"
-import {Text} from "@chakra-ui/react"
+import {HStack, List, ListItem, Text,Image} from "@chakra-ui/react"
+import getCroppedImage from "../services/image-url"
 
 const Genres = () => {
     const {Data,error}= useGenres()
   return (
     <>
       {error && <Text color={"red"}>{error}</Text>}
-      <ul>
-          {Data.map(genre =><li key={genre.id}>{genre.name}</li>)}
-      </ul>
+      <List>
+          {Data.map(genre =>
+          <ListItem key={genre.id} paddingY={'5px'}>
+            <HStack>
+              <Image boxSize='32px' src={getCroppedImage(genre.image_background)} borderRadius={8}></Image>
+            <Text fontSize={"lg"}>{genre.name}</Text>
+            </HStack>
+            </ListItem>)}
+      </List>
     </>
     )
 }
