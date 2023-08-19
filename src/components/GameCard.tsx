db2111b7f6de077a64d4d0beda5e84f7d3ac719d
DEROUICH KARIM <106762744+MUGUWARA1@users.noreply.github.com>
2023-08-19 16:25:53 +0100

    clean up the gameCards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,11 +14,11 @@ const GameCard = ({game}:Props) => {
     <Card>
         <Image src={getCroppedImage(game.background_image)}></Image>
         <CardBody>
-            <Heading fontSize={"2xl"}>{game.name}</Heading>
-            <HStack justifyContent={'space-between'}>
+            <HStack justifyContent={'space-between'} marginBottom={3}>
                 <IconList platforms={game.parent_platforms.map(p => p.platform)}></IconList>
                 <MetacriticIcon score={game.metacritic}></MetacriticIcon>
             </HStack>
+            <Heading fontSize={"2xl"}>{game.name}</Heading>
         </CardBody>
     </Card>
   )
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,7 +19,7 @@ const GameGrid = ({gameQuery}:Props) => {
       {error && <Text color={"red"}>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
-        spacing={4}
+        spacing={6}
         padding={"10px"}
         justifyContent={"Center"}
       >
